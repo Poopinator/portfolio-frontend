bff46a3a682dfab914d59332a67c4b1db4e2bc7d
poopinator <wuzheming1@gmail.com>
2025-10-23 13:55:41 +0800

    testing new

--- src/main.js
+++ src/main.js
@@ -2,22 +2,19 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { createI18n } from 'vue-i18n';
-import en from '@/locales/en.json';
-import zh from '@/locales/zh.json';
-
-
+import { createI18n } from "vue-i18n";
+import en from "@/locales/en.json";
+import zh from "@/locales/zh.json";
 
 const i18n = createI18n({
   legacy: false,
-  locales: 'en', // default
-  fallbackLocale: 'en',
-  messages: { en, zh }
+  locale: "en",
+  fallbackLocale: "en",
+  messages: { en, zh },
 });
 
-const app = createApp(App);
 
-// if you use routing (HomeView, AboutView, etc.)
+const app = createApp(App);
 app.use(router);
 app.use(i18n);
 app.mount("#app");
